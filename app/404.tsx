98c744f738b7b5878ce6fa93af3f17f16a9b8eac
Siraj Ahmed <166845007+sirajahmedx@users.noreply.github.com>
2025-09-10 09:50:09 +0500

    add custom 404 page

---
+++ app/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="text-lg mt-4">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
